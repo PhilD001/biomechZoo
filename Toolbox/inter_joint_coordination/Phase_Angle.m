726e1cf713f1518b4679c4ed8a4fa87798fefc10
Philippe Dixon <>
2020-04-01 09:33:59 -0400

    add files from Patrick

---
+++ Toolbox/inter_joint_coordination/Phase_Angle.m
@@ -0,0 +1,41 @@
+function [PA_data] = Phase_Angle(angle,event1,event2)
+
+%This function determines the Phase Angle for a single kinematic waveform
+%using the Hilbert transform method.  Event1 and Event2 correspond to
+%segment of interest (to incorporate data padding if required)
+
+if nargin == 3
+    temp_length=(event1:event2);
+    data=(angle(temp_length));
+    L=length(data);
+    
+    %1) Centre data around 0
+    cdata = data - min(data) - (max(data)-min(data))/2;
+
+    % 2) Hilbert transform
+    X=hilbert(cdata);
+
+    % 3) Phase Angle
+    PA_data=zeros(1,L);
+        for i=1:L
+        PA_data(i)=atan2(imag(X(i)),real(X(i)));
+        end
+    PA_data=rad2deg(PA_data);
+else
+    data=angle;
+    L=length(data);
+    
+    %1) Centre data around 0
+    cdata = data - min(data) - (max(data)-min(data))/2;
+
+    % 2) Hilbert transform
+    X=hilbert(cdata);
+
+    % 3) Phase Angle
+    PA_data=zeros(1,L);
+        for i=1:L
+        PA_data(i)=atan2(imag(X(i)),real(X(i)));
+        end
+    PA_data=rad2deg(PA_data);
+end
+
---
+++ Toolbox/inter_joint_coordination/Practice_Phil.m
@@ -0,0 +1,43 @@
+%% This is set up to use Hip and Knee data
+
+%% Step 1: Determine Phase Angles
+
+figure;hold on
+
+for i=1:size(left_hip_stk,1)
+    data=left_hip_stk(i,:);
+    PA_hip(i,:)=Phase_Angle(data,32,94);
+    plot(PA_hip')
+    title('Phase Angle Hip')
+end
+
+figure; hold on
+
+for i=1:size(left_knee_stk,1)
+    data=left_knee_stk(i,:);
+    PA_knee(i,:)=Phase_Angle(data,32,94);
+    plot(PA_knee')
+    title('Phase Angle Knee')
+end
+
+%% Step 2: Determine CRP
+
+for i=1:size(PA_hip,1)
+    data_prox=PA_hip(i,:);
+    data_dist=PA_knee(i,:);
+    CRP_Knee_Hip(i,:)=CRP(data_dist,data_prox);
+end
+
+%% Step 3: Determine CRP stats
+
+figure; hold on
+
+plot(CRP_Knee_Hip')
+title('CRP Angle Knee-Hip')
+
+[MARP,DP]=CRP_stats(CRP_Knee_Hip);
+figure;hold on
+title('MARP and DP')
+plot(MARP, 'LineWidth',2)
+plot(DP, 'LineWidth',2)
+legend ('MARP','DP')
