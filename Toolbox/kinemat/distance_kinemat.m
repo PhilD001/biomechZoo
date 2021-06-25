e8ea00d402f5f2f08a2a5fb7859c3eda0c969687
Phil Dixon <PhilD001@users.noreply.github.com>
2021-06-25 15:20:26 -0400

    issue #21 resolved

--- Toolbox/kinemat/distance_kinemat.m
+++ Toolbox/kinemat/distance_kinemat.m
@@ -1,4 +1,4 @@
-function [dist]=distance_kinemat(XYZmarker1,XYZmarker2)
+function dist = distance_kinemat(XYZmarker1,XYZmarker2)
 % function [dist]=distance(XYZmarker1,XYZmarker2)
 % Description:	Calculates the distance between two markers.
 % Input:	XYZmarker1: [X,Y,Z] coordinates of marker 1
@@ -9,17 +9,22 @@ function [dist]=distance_kinemat(XYZmarker1,XYZmarker2)
 % Date:		November, 1996
 % Last Changes:	November 29, 1996
 % Version:	1.0
-		
+%
+% Updated by Philippe C. Dixon June 25th, 2021
+% - replaced break statement in if loop for an error raise
+% - cleaned up code to remove matlab syntax warnings
+%
+%
+% See also magnitude
 
 [s1,t1]=size(XYZmarker1);
 [s2,t2]=size(XYZmarker2);
 
-if s1~=s2 | t1~=t2 | t2~=3
- disp('The input matrices must have 3 columns and the same number')
- disp('of rows. Try again.')
- break
+if s1~=s2 || t1~=t2 || t2~=3
+    error('The input matrices must have 3 columns and the same number of rows')
 end
 
-tmp=[XYZmarker1-XYZmarker2].^2;
-dist=[sum(tmp')'].^0.5;
+tmp= (XYZmarker1-XYZmarker2).^2;
+dist=(sum(tmp, 2)).^0.5;
+
 
