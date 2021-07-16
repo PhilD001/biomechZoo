0525c298e03f97d38cd4cb553d6cb498ad03398c
Phil Dixon <PhilD001@users.noreply.github.com>
2021-07-16 13:06:25 -0400

    add Machine Learning module

--- checkZoo.m
+++ checkZoo.m
@@ -2,11 +2,21 @@ function checkZoo
 
 % CHECKZOO should be used before pushing new updates to the repositories
 
-% 1 - run all examples test
+
+
+% 1 - check machine learning module
+ml_processing_template_IMU_simple
+ml_processing_template_IMU
+ml_processing_template_mocap
+
+% 2 - run all examples test
 all_examples_test
 
-% 2 - run sample study
+% 3 - run sample study
 samplestudy_process
+
+
+
 
 
 
