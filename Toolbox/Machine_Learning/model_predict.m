deea21f8bff9d913ef301c77cfca1ea57e65cd80
Phil Dixon <PhilD001@users.noreply.github.com>
2022-04-25 11:19:22 -0400

    Update model_predict.m

--- Toolbox/Machine_Learning/model_predict.m
+++ Toolbox/Machine_Learning/model_predict.m
@@ -1,1 +1,7 @@
-function y_pred = model_predict(mdl, model_name, x_test);
+function y_pred = model_predict(mdl, model_name, x_test)
+
+if contains(model_name,{'FF','LSTM','BILS','CNN'})
+    y_pred = classify(mdl, x_test);
+else
+    y_pred= predict(mdl,x_test);
+end
