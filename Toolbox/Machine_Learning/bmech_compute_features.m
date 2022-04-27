526f65115bf5caf6ee68362c09153a44e4283c50
Phil Dixon <philippe.dixon@gmail.com>
2022-04-27 10:26:53 -0400

    rename bmech_compute_feature to bmech_compute_features

--- Toolbox/Machine_Learning/bmech_compute_features.m
+++ Toolbox/Machine_Learning/bmech_compute_features.m
@@ -1,6 +1,6 @@
-function event_names = bmech_compute_feature(fld, ch, method)
+function event_names = bmech_compute_features(fld, ch, method)
 
-% BMECH_FEATURE_EXTRACTION extracts basic statistical features from line data 
+% bmech_compute_features extracts basic statistical features from line data 
 %
 % ARGUMENTS
 %  fld           ...   string, folder to batch process.   
