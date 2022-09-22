fb4e9d201e50410dbfce7ca7413132336efb7caf
Cloé <clo3003@hotmail.com>
2022-09-22 14:50:47 -0400

    change

--- Toolbox/inter_joint_coordination/bmech_phase_angle.m
+++ Toolbox/inter_joint_coordination/bmech_phase_angle.m
@@ -1,19 +1,10 @@
-function bmech_phase_angle(fld, chns, evt1, evt2)
+function bmech_phase_angle(fld, chns)
 
 % BMECH_PHASE_ANGLE(fld, evt1, evt2) computes phase angles between evt1 and evt2 for channels
 %
 % ARGUMENTS
 %  fld        ...  Folder to batch process (string). 
 %  chns       ...  Channels for which to compute phase angle. Channels must be 1 x n (exploded)
-%  event1     ...   start of section of interest for angle data
-%  event2     ...   end of section of interest for angle data
-
-
-% error checking
-if nargin < 3
-    evt1 = [];
-    evt2 = [];  
-end
 
 
 % Batch process
@@ -23,7 +14,7 @@ fl = engine('path',fld,'extension','zoo');
 for i = 1:length(fl)
     data = zload(fl{i});
     batchdisp(fl{i},'computing phase angle');
-    data = phase_angle_data(data, evt1, evt2, chns);
+    data = phase_angle_data(data, chns);
     zsave(fl{i},data);
 end
 
