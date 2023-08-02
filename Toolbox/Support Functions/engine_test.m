97dc02eecd768a697db17e8f91052b386af8cf59
oussema-dev <oussama.jlassi@hotmail.fr>
2023-08-02 16:04:10 -0400

    add comments

--- Toolbox/Support Functions/engine_test.m
+++ Toolbox/Support Functions/engine_test.m
@@ -4,13 +4,13 @@ fld = 'D:\Lab\Thesis\chapters'
 tic
 % Record the current memory usage
 memory_before = whos;
-
-fl = engine('path',fld, 'extension', 'pptx', 'search path', 'results')
-% Stop timer and display elapsed time
+% fl = engine('path',fld, 'extension', 'pptx', 'search path', 'results')/
+fl = engine_improved('path',fld, 'extension', 'pptx', 'search path', 'results')
+% Stop timer
 elapsed_time = toc;
 % Record the memory usage after function execution
 memory_after = whos;
 disp(['Elapsed time: ', num2str(elapsed_time), ' seconds']);
-% Calculate the difference in memory usage
+% Calculate and display the difference in memory usage
 memory_consumed = sum([memory_after.bytes]) - sum([memory_before.bytes]);
 disp(['Memory consumed: ', num2str(memory_consumed / (1024^2)), ' MB']);
